2bc3f9c0e11dd82ab7283046a14288aa68bdccb0
Carlos Maximiliano <maxi.krbr@gmail.com>
2022-06-16 11:52:29 -0300

    added calculations

---
+++ script.js
@@ -0,0 +1,26 @@
+const preçoRefrigerante = 11.99
+const preçoMacarrao = 6.99
+const preçoErvilha = 6.99
+const preçoArroz = 22.99
+const preçoFeijão = 11.89
+const preçoVinho = 70.00
+
+let totalRefri = preçoRefrigerante * 3
+let totalMacarrao = preçoMacarrao * 4
+let totalErvilha = preçoErvilha * 3
+let totalArroz = preçoArroz * 3
+let totalFeijão = preçoFeijão * 2
+let totalVinho = preçoVinho * 3
+
+total = totalRefri + totalMacarrao + totalErvilha + totalArroz + totalFeijão + totalVinho
+
+if (total % 2 == 0) {
+    let voce = metade - vinho;
+    let amigo = metade + vinho;
+}
+else {
+    let você = metade;
+    let amigo = metade;
+}
+
+console.log("O valor total da compra é ${total}. Você deve pagar ${voce} e seu amigo deve pagar ${amigo}")
